3d8d381bdef80f453f526f7bdece0f43b4bc48e3
Parniuk Maryia <107621519+cinnaamonka@users.noreply.github.com>
2024-03-08 14:03:04 +0100

    optimized a bit, but still looks ugly

--- Minigin/MyImgui.cpp
+++ Minigin/MyImgui.cpp
@@ -1,6 +1,7 @@
 #include "../pch.h"
 
 std::vector<float> RunArrayComputationBenchmark(int arraySize, int numIterations);
+void DrawBasicWindow(bool* p_open, const char* name);
 template<typename T>
 std::vector<float> RunObject3DComputation(int arraySize, int numIterations, T* arr)
 {
@@ -107,38 +108,27 @@ static uint32_t selection_start = 0, selection_length = 0;
 static char inputTextBuffer[256] = "";
 static char inputSecondTextBuffer[256] = "";
 
-
-void DrawPlot(std::vector<float> benchmarkResults)
+static void DrawPlot(const float* data[], int size, int amount = 1)
 {
-	// Draw first plot with multiple sources
 	ImGui::PlotConfig conf;
-	conf.values.ys = benchmarkResults.data();
-	conf.values.count = static_cast<int>(benchmarkResults.size());
-	conf.values.colors = colors;
-	conf.scale.min = -1;
-	conf.scale.max = 20;
-	conf.tooltip.show = true;
-	conf.grid_x.show = true;
-	conf.grid_x.size = 60;
-	conf.grid_x.subticks = 4;
-	conf.grid_y.show = true;
-	conf.grid_y.size = 0.5f;
-	conf.grid_y.subticks = 5;
-	conf.selection.show = true;
-	conf.selection.start = &selection_start;
-	conf.selection.length = &selection_length;
-	conf.frame_size = ImVec2(200, 100);
+	if (amount > 1)
+	{
+		const float* y_data[] =
+		{
+		  data[0],
+		  data[1]
+		};
 
-	ImGui::Plot("plot1", conf);
-}
-void DrawDoublePlot(std::vector<float> benchmarkResults, std::vector<float> benchmarkResults2)
-{
-    const float* y_data[] = { benchmarkResults.data(), benchmarkResults2.data()};
-
-	ImGui::PlotConfig conf;
-	conf.values.count = static_cast<int>(benchmarkResults.size());
-	conf.values.ys_list = y_data; // use ys_list to draw several lines simultaneously
-	conf.values.ys_count = 2;
+		conf.values.ys_list = y_data;
+		conf.values.ys_count = 2;
+	}
+	else
+	{
+		conf.values.ys = data[0];
+	}
+	
+	conf.values.count = size;
+	
 	conf.values.colors = colors;
 	conf.scale.min = -1;
 	conf.scale.max = 20;
@@ -158,46 +148,10 @@ void DrawDoublePlot(std::vector<float> benchmarkResults, std::vector<float> benc
 
 void ImGui::ShowMyWindow(bool* p_open)
 {
-	IM_ASSERT(ImGui::GetCurrentContext() != NULL && "Missing Dear ImGui context. Refer to examples app!");
-
-	const ImGuiViewport* main_viewport = ImGui::GetMainViewport();
-	ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x + 100, main_viewport->WorkPos.y + 20), ImGuiCond_FirstUseEver);
-	ImGui::SetNextWindowSize(ImVec2(300, 300), ImGuiCond_FirstUseEver);
-
-	static bool no_titlebar = false;
-	static bool no_scrollbar = false;
-	static bool no_menu = false;
-	static bool no_move = false;
-	static bool no_resize = false;
-	static bool no_collapse = false;
-	static bool no_close = false;
-	static bool no_nav = false;
-	static bool no_background = false;
-	static bool no_bring_to_front = false;
-	static bool unsaved_document = false;
-
-	ImGuiWindowFlags window_flags = 0;
-	if (no_titlebar)        window_flags |= ImGuiWindowFlags_NoTitleBar;
-	if (no_scrollbar)       window_flags |= ImGuiWindowFlags_NoScrollbar;
-	if (!no_menu)           window_flags |= ImGuiWindowFlags_MenuBar;
-	if (no_move)            window_flags |= ImGuiWindowFlags_NoMove;
-	if (no_resize)          window_flags |= ImGuiWindowFlags_NoResize;
-	if (no_collapse)        window_flags |= ImGuiWindowFlags_NoCollapse;
-	if (no_nav)             window_flags |= ImGuiWindowFlags_NoNav;
-	if (no_background)      window_flags |= ImGuiWindowFlags_NoBackground;
-	if (no_bring_to_front)  window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
-	if (unsaved_document)   window_flags |= ImGuiWindowFlags_UnsavedDocument;
-	if (no_close)           p_open = NULL;
+	DrawBasicWindow(p_open, "Exercise 1");
 
 	static int resultStep;
 	static std::vector<float> benchmarkResults;
-
-
-	if (!ImGui::Begin("Exercise 1", p_open, window_flags))
-	{
-		ImGui::End();
-		return;
-	}
 
 	ImGui::PushItemWidth(ImGui::GetFontSize() * -12);
 
@@ -260,55 +214,21 @@ void ImGui::ShowMyWindow(bool* p_open)
 
 	if (benchmarkResults.size() > 0)
 	{
-		DrawPlot(benchmarkResults);
+		const float* data[] = { benchmarkResults.data() };
+		DrawPlot(data,static_cast<int>(benchmarkResults.size()));
 	}
 	ImGui::PopItemWidth();
 	ImGui::End();
 }
 void ImGui::ShowSecondWindow(bool* p_open)
 {
-	IM_ASSERT(ImGui::GetCurrentContext() != NULL && "Missing Dear ImGui context. Refer to examples app!");
-
-	const ImGuiViewport* main_viewport = ImGui::GetMainViewport();
-	ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x + 100, main_viewport->WorkPos.y + 20), ImGuiCond_FirstUseEver);
-	ImGui::SetNextWindowSize(ImVec2(300, 300), ImGuiCond_FirstUseEver);
-
-	static bool no_titlebar = false;
-	static bool no_scrollbar = false;
-	static bool no_menu = false;
-	static bool no_move = false;
-	static bool no_resize = false;
-	static bool no_collapse = false;
-	static bool no_close = false;
-	static bool no_nav = false;
-	static bool no_background = false;
-	static bool no_bring_to_front = false;
-	static bool unsaved_document = false;
-
-	ImGuiWindowFlags window_flags = 0;
-	if (no_titlebar)        window_flags |= ImGuiWindowFlags_NoTitleBar;
-	if (no_scrollbar)       window_flags |= ImGuiWindowFlags_NoScrollbar;
-	if (!no_menu)           window_flags |= ImGuiWindowFlags_MenuBar;
-	if (no_move)            window_flags |= ImGuiWindowFlags_NoMove;
-	if (no_resize)          window_flags |= ImGuiWindowFlags_NoResize;
-	if (no_collapse)        window_flags |= ImGuiWindowFlags_NoCollapse;
-	if (no_nav)             window_flags |= ImGuiWindowFlags_NoNav;
-	if (no_background)      window_flags |= ImGuiWindowFlags_NoBackground;
-	if (no_bring_to_front)  window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
-	if (unsaved_document)   window_flags |= ImGuiWindowFlags_UnsavedDocument;
-	if (no_close)           p_open = NULL;
+	DrawBasicWindow(p_open, "Exercise 2");
 
 	static int resultStep;
 	static std::vector<float> benchmarkResults;
 	static std::vector<float> benchmarkResults2;
 
 	const int arraySize = 1 << 26;
-
-	if (!ImGui::Begin("Exercise 2", p_open, window_flags))
-	{
-		ImGui::End();
-		return;
-	}
 
 	ImGui::PushItemWidth(ImGui::GetFontSize() * -12);
 
@@ -400,7 +320,8 @@ void ImGui::ShowSecondWindow(bool* p_open)
 
 	if (benchmarkResults.size() > 0)
 	{
-		DrawPlot(benchmarkResults);
+		const float* data[] = { benchmarkResults.data() };
+		DrawPlot(data, static_cast<int>(benchmarkResults.size()));
 	}
 	if (!shouldWaitThirdExercise)
 	{
@@ -412,13 +333,16 @@ void ImGui::ShowSecondWindow(bool* p_open)
 
 	if (benchmarkResults2.size() > 0)
 	{
-		DrawPlot(benchmarkResults2);
+		const float* data[] = {benchmarkResults2.data() };
+		DrawPlot(data, static_cast<int>(benchmarkResults2.size()));
 	}
 
 	// combined 
 	if (benchmarkResults2.size() > 0 && benchmarkResults.size() > 0)
 	{
-		DrawDoublePlot(benchmarkResults, benchmarkResults2);
+		const float* data[] = { benchmarkResults.data() ,benchmarkResults2.data() };
+
+		DrawPlot(data, static_cast<int>(benchmarkResults.size()),2);
 	}
 	ImGui::PopItemWidth();
 	ImGui::End();
@@ -488,5 +412,44 @@ std::vector<float> RunArrayComputationBenchmark(int arraySize, int numIterations
 	delete[] arr;
 	return finalAverageTimings;
 }
+void DrawBasicWindow(bool* p_open,const char* name)
+{
+	IM_ASSERT(ImGui::GetCurrentContext() != NULL && "Missing Dear ImGui context. Refer to examples app!");
 
+	const ImGuiViewport* main_viewport = ImGui::GetMainViewport();
+	ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x + 100, main_viewport->WorkPos.y + 20), ImGuiCond_FirstUseEver);
+	ImGui::SetNextWindowSize(ImVec2(300, 300), ImGuiCond_FirstUseEver);
+
+	static bool no_titlebar = false;
+	static bool no_scrollbar = false;
+	static bool no_menu = false;
+	static bool no_move = false;
+	static bool no_resize = false;
+	static bool no_collapse = false;
+	static bool no_close = false;
+	static bool no_nav = false;
+	static bool no_background = false;
+	static bool no_bring_to_front = false;
+	static bool unsaved_document = false;
+
+	ImGuiWindowFlags window_flags = 0;
+	if (no_titlebar)        window_flags |= ImGuiWindowFlags_NoTitleBar;
+	if (no_scrollbar)       window_flags |= ImGuiWindowFlags_NoScrollbar;
+	if (!no_menu)           window_flags |= ImGuiWindowFlags_MenuBar;
+	if (no_move)            window_flags |= ImGuiWindowFlags_NoMove;
+	if (no_resize)          window_flags |= ImGuiWindowFlags_NoResize;
+	if (no_collapse)        window_flags |= ImGuiWindowFlags_NoCollapse;
+	if (no_nav)             window_flags |= ImGuiWindowFlags_NoNav;
+	if (no_background)      window_flags |= ImGuiWindowFlags_NoBackground;
+	if (no_bring_to_front)  window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
+	if (unsaved_document)   window_flags |= ImGuiWindowFlags_UnsavedDocument;
+	if (no_close)           p_open = NULL;
+
+
+	if (!ImGui::Begin(name, p_open, window_flags))
+	{
+		ImGui::End();
+		return;
+	}
+}
 
